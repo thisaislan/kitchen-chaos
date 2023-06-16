e31ad5ed8759c36c7bd8538e889c8403e98c8c3a
thisaislan <this.aislan@gmail.com>
2023-06-15 23:23:55 -0300

    💫 Animatio: Add player animations

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSped = 7f;
     [SerializeField] private float rotateSpeed = 10f;
 
+    private bool isWalking;
+
     private void Update()
     {
         var inputVector = Vector2.zero;
@@ -25,10 +27,16 @@ public class Player : MonoBehaviour
         transform.position += movementVector * moveSped * Time.deltaTime;
 
         SetRotation(movementVector);
+        SetWalkingState(movementVector != Vector3.zero);
     }
-
 
     private void SetRotation(Vector3 direction) =>
         transform.forward = Vector3.Slerp(transform.forward, direction, Time.deltaTime * rotateSpeed);
+
+    private void SetWalkingState(bool isWalking) =>
+        this.isWalking = isWalking;
+
+    public bool IsWalking() =>
+        isWalking;
 
 }
---
+++ KitchenChaos/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Player player;
+    [SerializeField] private Animator animator;
+
+    private void Update() =>
+        animator.SetBool(IS_WALKING, player.IsWalking());
+
+}
