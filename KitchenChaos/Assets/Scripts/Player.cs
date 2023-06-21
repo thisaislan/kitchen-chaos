c7668159da9dde22d35e006a5dcca91fbd6c78f1
thisaislan <this.aislan@gmail.com>
2023-06-21 19:49:09 -0300

    ✨ Feature: Add collision

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] private float moveSped = 7f;
+    [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float playerRadius = 0.7f;
+    [SerializeField] private float playerHeight = 2f;
 
     [SerializeField] private GameInput gameInput;
 
@@ -15,12 +17,43 @@ public class Player : MonoBehaviour
 
     private void SetMovement(Vector2 inputVector) 
     {
+        // refactor this
         var movementVector = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        transform.position += movementVector * moveSped * Time.deltaTime;
+        var moveDistance = moveSpeed * Time.deltaTime;
 
         SetRotation(movementVector);
-        SetWalkingState(movementVector != Vector3.zero);
+
+        var canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementVector, moveDistance);
+
+        if (!canMove)
+        {
+            var moveDirX = new Vector3(movementVector.x, 0, 0).normalized;
+            
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                movementVector = moveDirX;
+            }
+            else
+            {
+                var moveDirZ = new Vector3(0, 0, movementVector.z).normalized;
+
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    movementVector = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        { 
+            transform.position += movementVector * moveSpeed * Time.deltaTime;
+            SetWalkingState(movementVector != Vector3.zero);
+        }
+
     }
 
     private void SetRotation(Vector3 direction) =>
