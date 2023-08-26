10a2189b3490ab4a7e81c9db45525edb37d0336c
thisaislan <this.aislan@gmail.com>
2023-08-26 20:21:39 -0300

    ✨ Feature: Sync player animations

--- KitchenChaos/Assets/Scripts/PlayerAnimator.cs
+++ KitchenChaos/Assets/Scripts/PlayerAnimator.cs
@@ -1,13 +1,19 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
     [SerializeField] private Animator animator;
     [SerializeField] private Player player;
 
-    private void Update() =>
-        animator.SetBool(IS_WALKING, player.IsWalking);
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            animator.SetBool(IS_WALKING, player.IsWalking);
+        }
+    }
 
 }
