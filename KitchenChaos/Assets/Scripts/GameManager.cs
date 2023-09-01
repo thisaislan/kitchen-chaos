de76c0429897e368c96b6809d44643f60b79d5f9
thisaislan <this.aislan@gmail.com>
2023-08-31 23:06:36 -0300

    🐛 Fix: GameManager temp start point

--- KitchenChaos/Assets/Scripts/GameManager.cs
+++ KitchenChaos/Assets/Scripts/GameManager.cs
@@ -41,7 +41,7 @@ public class GameManager : MonoBehaviour
         GameInput.Instance.OnInteractAction += OnGameInputInteractAction;
 
         // TODO - To start game automatically
-        State = GameState.CountdownToStart;
+        State = GameState.WaitingToStart;
     }
 
     private void OnDestroy()
